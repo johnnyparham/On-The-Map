bfe73dbfe709631b6253220189f07d5e257c9e74
johnnyparham <johnny.parham@gmail.com>
2016-06-08 16:27:38 -0400

    Setup LoginViewController Scene in Main Storyboard

---
+++ LoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.swift
+//  OnTheMap
+//
+//  Created by Johnny Parham on 6/8/16.
+//  Copyright © 2016 Johnny Parham. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+}
