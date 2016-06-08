a4c87122010a995893185964f97898d50c5d196a
johnnyparham <johnny.parham@gmail.com>
2016-06-08 16:37:18 -0400

    Wired up the Outlets and Actions in LoginViewController

--- LoginViewController.swift
+++ LoginViewController.swift
@@ -9,5 +9,40 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    // MARK: -
+    // MARK: Components
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    // MARK: -
+    // MARK: Action Functions
+    
+    // Login using the Udacity API
+    @IBAction func loginAction(sender: AnyObject) {
+        
+    }
+    
+    
+    
+    // Login using the Facebook API
+    @IBAction func loginWithFacebook(sender: AnyObject) {
+        
+    }
+    
+    
+    
+    
+    // Sign up on the Udacity registration site
+    @IBAction func signUpAction(sender: AnyObject) {
+        
+    }
 
 }
