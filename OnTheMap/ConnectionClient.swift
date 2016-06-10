1be146276167026c9baf1e1078dc488b12654a9f
johnnyparham <johnny.parham@gmail.com>
2016-06-10 16:18:14 -0400

    Setup ConnectionClient Class

---
+++ OnTheMap/ConnectionClient.swift
@@ -0,0 +1,166 @@
+//
+//  ConnectionClient.swift
+//  OnTheMap
+//
+//  Created by Johnny Parham on 6/10/16.
+//  Copyright © 2016 Johnny Parham. All rights reserved.
+//
+
+import Foundation
+
+class ConnectionClient: NSObject {
+    
+    // Shared session
+    var session: NSURLSession
+    
+    // Facebook manager
+    let facebookManager = FBSDKLoginManager()
+    
+    // Singleton instance because all the network flows will need this class to do the operations
+    class func sharedInstance() -> ConnectionClient {
+        
+        struct Singleton {
+            static var sharedInstance = ConnectionClient()
+        }
+        
+        return Singleton.sharedInstance
+        
+    }
+    
+    // Initialize the class
+    override init() {
+        session = NSURLSession.sharedSession()
+        super.init()
+    }
+    
+    // MARK: -
+    // MARK: POST Method
+    func doPOSTWithMethod(method: String!, ofBaseUrl baseUrl: String!, withRequestContent content: [String:String], andWithBody body: String!, isFromUdacity isUdacity: Bool?, completionHandler: (result: AnyObject!, error: NSError?) -> Void) {
+        
+        let urlString = baseUrl + method
+        let url = NSURL(string: urlString)
+        
+        let request = NSMutableURLRequest(URL: url!)
+        request.HTTPMethod = "POST"
+        
+        for(key, value) in content {
+            request.addValue(value, forHTTPHeaderField: key)
+        }
+        
+        request.HTTPBody = body!.dataUsingEncoding(NSUTF8StringEncoding)!
+        
+        session.dataTaskWithRequest(request) { (data, result, error) in
+            
+            // verify if any error occurs
+            guard error == nil else {
+                completionHandler(result: nil, error: error)
+                return
+            }
+            
+            // remove the first 5 charactoers from Udacity response
+            var newData = data
+            
+            if isUdacity! {
+                newData = data?.subdataWithRange(NSMakeRange(5, (data?.length)! - 5))
+            }
+            
+            let json: AnyObject!
+            
+            do {
+                json = try NSJSONSerialization.JSONObjectWithData(newData!, options: NSJSONReadingOptions.AllowFragments)
+            } catch {
+                json = nil
+            }
+            
+            // send the response to be handled outside
+            completionHandler(result: json, error: nil)
+        }.resume()
+    }
+    
+    // MARK: -
+    // MARK: GET Method
+    func doGETWithMethod(method: String!, ofBaseUrl baseUrl: String!, withRequestContent content: [String:String], isFromUdacity isUdacity: Bool?, completionHandler: (result: AnyObject!, error: NSError?) -> Void) {
+        
+        let urlString = baseUrl + method
+        let url = NSURL(string: urlString)
+        
+        let request = NSMutableURLRequest(URL: url!)
+        for(key, value) in content {
+            request.addValue(value, forHTTPHeaderField: key)
+        }
+        
+        session.dataTaskWithRequest(request) { (data, result, error) in
+            
+            // verify if any error occurs
+            guard error == nil else {
+                completionHandler(result: nil, error: error)
+                return
+            }
+            
+            // remove the first 5 characters from Udacity response
+            var newData = data
+            
+            if isUdacity! {
+                newData = data?.subdataWithRange(NSMakeRange(5, (data?.length)! - 5))
+            }
+            
+            let json: AnyObject!
+            
+            do {
+                json = try NSJSONSerialization.JSONObjectWithData(newData!, options: NSJSONReadingOptions.AllowFragments)
+            } catch {
+                json = nil
+            }
+            
+            // send the response to be handled outside
+            completionHandler(result: json, error: nil)
+            
+        }.resume()
+    }
+    
+    // MARK: -
+    // MARK: DELETE Method
+    func doDELETEWithMethod(method: String!, ofBaseUrl baseUrl: String!, completionHandler: (result: AnyObject!, error: NSError?) -> Void) {
+        
+        let urlString = baseUrl + method
+        let url = NSURL(string: urlString)
+        
+        let request = NSMutableURLRequest(URL: url!)
+        request.HTTPMethod = "DELETE"
+        
+        var xsrfCookie: NSHTTPCookie? = nil
+        let sharedCookieStorage = NSHTTPCookieStorage.sharedHTTPCookieStorage()
+        
+        for cookie in sharedCookieStorage.cookies! {
+            if cookie.name == "XSRF-TOKEN" { xsrfCookie = cookie }
+        }
+        
+        if let xsrfCookie = xsrfCookie {
+            request.setValue(xsrfCookie.value, forHTTPHeaderField: "X-XSRF-TOKEN")
+        }
+        
+        session.dataTaskWithRequest(request) { (data, result, error) in
+            
+            // verify if any error occurs
+            guard error == nil else {
+                completionHandler(result: nil, error: error)
+                return
+            }
+            
+            let newData = data?.subdataWithRange(NSMakeRange(5, (data?.length)! - 5))
+            let json: AnyObject!
+            
+            do {
+                json = try NSJSONSerialization.JSONObjectWithData(newData!, options: NSJSONReadingOptions.AllowFragments)
+            } catch {
+                json = nil
+            }
+            
+            // send the response to be handled outside
+            completionHandler(result: json, error: nil)
+            
+        }.resume()
+    }
+    
+
+}
